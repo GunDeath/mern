b869337236712658076af3191de96b654e481503
Phill <phillbleck@gmail.com>
2020-03-17 15:54:27 +0300

    Finish login module

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,11 @@
 const {check, validationResult} = require('express-validator');
-const hash_password = require('bcryptjs');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 const {Router} = require('express');
+const bcrypt = require('bcryptjs');
+const config = require('config');
 
 const router = Router();
-
 
 // /api/auth/register
 router.post(
@@ -33,7 +34,7 @@ router.post(
             return  res.status(400).json({message: "User with this email already been created"})
          }
 
-         const hashedPassword = await hash_password.hash(password, 12);
+         const hashedPassword = await bcrypt.hash(password, 12);
          const user = new User({ email, password: hashedPassword});
 
          await user.save();
@@ -46,8 +47,47 @@ router.post(
 });
 
 // /api/auth/login
-router.post('/login', async (req, res) =>{
+router.post(
+    '/login',
+    [
+        check('email', 'Entry correct email').normalizeEmail().isEmail(),
+        check('password', 'Entry password').exists()
+    ],
+    async (req, res) =>{
+        try{
+            const errors = validationResult(req);
 
+            if(!errors.isEmpty()){
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: "Incorrect email, or password of login"
+                })
+            }
+
+            const {email, password} = req.body;
+
+            const user = await User.findOne({email});
+
+            if(!user){
+                return res.status(400).json({message: 'User with this email was not found'})
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if(!isMatch){
+                return  res.status(400).json({ message: 'User with this password was not found'})
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                config.get('jwtSecret'),
+                {expiresIn: '1h'}
+            );
+
+            res.json({ token, userId: user.id })
+        }catch (e) {
+            res.status(500).json({message: 'Register. Smth wrong'})
+        }
 });
 
 module.exports = router;
